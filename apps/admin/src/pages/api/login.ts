970e0fceb9c05949a0920f0dc8f7d963e8ab7c0f
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2023-09-21 12:09:59 +0530

    Changed admin login api response message.

--- apps/admin/src/pages/api/login.ts
+++ apps/admin/src/pages/api/login.ts
@@ -9,7 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         if(req.headers.username && req.headers.password){
             if(req.headers.username==process.env.ADMIN_USERNAME && req.headers.password==process.env.ADMIN_PASSWORD){
                 var token = jwt.sign({username:req.body.username, role: 'admin'}, process.env.JWT_SECRET as string, {expiresIn:'1h'});
-                res.status(200).json({ message: 'Admin created successfully', token: token });
+                res.status(200).json({ message: 'Admin logged in successfully', token: token });
             }
             else{
                 res.status(401).json({'Error':'Invalid credentials'});
